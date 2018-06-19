95c194570662de6ca15234b290440051af481ac3
tsmalla <tjark.smalla@gmx.de>
2018-06-19 10:20:09 +0200

    current state

--- App.js
+++ App.js
@@ -7,59 +7,93 @@
 import React, {Component} from 'react';
 import {
     Platform,
+    TouchableHighlight,
     StyleSheet,
     Text,
     View
 } from 'react-native';
 import {store} from "./src/store";
-import RadialChooser from "react-native-radial-chooser";
+//import RadialCounter from "react-native-radial-counter";
+import RadialCounter from "./src/components/RadialCounter";
 
-const instructions = Platform.select({
-    ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-    android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-
-export default class App extends Component<Props> {
+class MinuteSelector extends Component<Props> {
 
     constructor(props) {
         super(props);
+        this.diameter = 100;
         this.state = {
             timesRounded: 0,
-            ringProgress: 0.6,
+            ringProgress: 0.0,
+            showRadial: false
         };
     }
 
     render() {
+
+
         return (
-            <View style={styles.container}>
-                <RadialChooser longPressDelay={300}
-                               backgroundColor={"#e6e6e6"} tintColor={"#f77a52"}
-                               innerRadius={50} outerRadius={60}
-                               progress={this.state.ringProgress}
-                               timesRounded={this.state.timesRounded}
-                               onTimesRoundedChange={(timesRounded) =>
-                                   this.setState(o => {return {timesRounded}})}
-                               onRingProgressChange={(ringProgress) =>
-                                   this.setState(o => {return {ringProgress}})}/>
+            <View style={[styles.container, {width: this.diameter, height: this.diameter}]}>
+                <RadialCounter
+                    containerStyle={styles.svgContainer}
+                    longPressDelay={300}
+                    backgroundColor={"#e6e6e6"} tintColor={this.props.tint}
+                    innerRadius={this.diameter/2 - 10} outerRadius={this.diameter/2}
+                    progress={this.state.ringProgress}
+                    timesRounded={this.state.timesRounded}
+                    onTimesRoundedChange={(timesRounded) =>
+                        this.setState(o => {
+                            return {timesRounded: Math.max(timesRounded, 0)}
+                        })}
+                    onRingProgressChange={(ringProgress) =>
+                        this.setState(o => {
+                            return {ringProgress}
+                        })}
+                    buttonOptions={{
+                        useButton: true,
+                        buttonColor: this.props.tint,
+                        buttonActiveColor: this.props.tintDisabled,
+                        gap: 15,
+                        showRadialOnButton: true
+                    }}/>
+                <View pointerEvents={'none'} style={{alignItems: 'center'}}>
+                    <Text style={styles.countText}>{Math.round(this.state.timesRounded*30+this.state.ringProgress*30)}</Text>
+                    <Text style={styles.countText}>{"minutes"}</Text>
+                </View>
             </View>
         );
     }
 }
 
+
+export default class App extends Component<Props> {
+    render() {
+        return (
+            <View style={{flexDirection: "row", backgroundColor: '#f0f5f8', height: "100%",
+            justifyContent: "center", paddingTop: 30}}>
+                <MinuteSelector tint={"#459c20"} tintDisabled={"#acd8b0"}/>
+                <MinuteSelector tint={"#ab0f0e"} tintDisabled={"#ff5962"}/>
+                <MinuteSelector tint={"#4348f7"} tintDisabled={"#8881ff"}/>
+            </View>
+        )
+    }
+}
+
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
         justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
+        alignItems: 'center'
     },
     welcome: {
         fontSize: 20,
         textAlign: 'center',
         margin: 10,
+    },
+    svgContainer: {
+        position: "absolute"
+    },
+    countText: {
+        fontSize: 12,
+        color: "white"
     },
     instructions: {
         textAlign: 'center',
